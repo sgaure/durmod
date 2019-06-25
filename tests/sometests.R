0114b917a7a27655129f0c7b51943e51a5f3590f
Simen Gaure <simen@gaure.no>
2019-06-26 00:36:43 +0200

    Added more tests

--- tests/sometests.R
+++ tests/sometests.R
@@ -10,3 +10,15 @@ opt <- mphcrm(d ~ x1+x2+C(job,alpha)+D(duration)+S(alpha+1)+ID(id), data=dataset
 best <- opt[[1]]
 print(best)
 summary(best)
+
+# Then make a state factor
+state <- dataset[,factor(alpha+1,levels=1:2)]
+levels(state) <- c('unemp','onprogram')
+dataset$state <- state
+names(risksets) <- levels(state)
+dataset[, f := factor(sample(1:6,.N,replace=TRUE))]
+dataset[, g := factor(sample(1:4,.N,replace=TRUE))]
+opt <- mphcrm(d ~ x1+x2+C(job,alpha+f)+D(duration)+C(program,f*g) + S(alpha+1)+ID(id), data=dataset, 
+              risksets=risksets, control=mphcrm.control(threads=1,iters=1,callback=NULL))
+options(digits=3)
+summary(opt[[1]])
